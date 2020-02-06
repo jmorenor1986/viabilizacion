e123a2eea5f8ebfec0d1140e07c8c583d0efea55
MORENO ROJAS John Jairo <johnmorenoing@gmail.com>
2020-02-06 20:53:34 +0100

    Test ciudad service

--- validacion-ciudad-web/src/main/java/com/samtel/validation/common/exception/BadRequestException.java
+++ validacion-ciudad-web/src/main/java/com/samtel/validation/common/exception/BadRequestException.java
@@ -1,4 +1,4 @@
-package com.samtel.validation.common;
+package com.samtel.validation.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- viabilizacion-web/src/test/java/com/samtel/ports/secondary/solicitud/ValidarCiudadServiceTest.java
+++ viabilizacion-web/src/test/java/com/samtel/ports/secondary/solicitud/ValidarCiudadServiceTest.java
@@ -16,17 +16,14 @@ public class ValidarCiudadServiceTest {
     public static final String CODIGO_CIUDAD = "001";
     private ValidarCiudadService validarCiudadService;
 
-    @Mock
-    private ValidarCiudadRepository validarCiudadRepository;
 
     @Before
     public void setUp() {
-        validarCiudadService = new ValidarCiudadServiceImpl(validarCiudadRepository);
+        validarCiudadService = new ValidarCiudadServiceImpl();
     }
 
     @Test
     public void testValidarCiudadSuccess() {
-        Mockito.when(validarCiudadRepository.validarCodigoCiudad(CODIGO_CIUDAD)).thenReturn(Boolean.TRUE);
         Boolean result = validarCiudadService.validarCodigoCiudad(CODIGO_CIUDAD);
         Assert.assertEquals(result, Boolean.TRUE);
     }
