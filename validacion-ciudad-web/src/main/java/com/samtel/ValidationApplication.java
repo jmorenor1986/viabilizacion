24f9131027e9785143b92fdd55ade7ab05818f48
Jesus Sierra <jnsierrac@gmail.com>
2020-02-09 14:52:30 -0500

    Ajustes para el buen funcionamiento del microservicio ciudad

--- validacion-ciudad-web/src/main/java/com/samtel/ValidationApplication.java
+++ validacion-ciudad-web/src/main/java/com/samtel/ValidationApplication.java
@@ -3,6 +3,7 @@ package com.samtel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
