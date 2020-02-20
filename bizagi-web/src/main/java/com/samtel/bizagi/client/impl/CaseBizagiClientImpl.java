3c6bc11960cb3f85c4d4eb663b9d303914db56e4
MORENO ROJAS John Jairo <johnmorenoing@gmail.com>
2020-02-20 16:26:42 +0100

    Add test create case service

--- bizagi-web/src/main/java/com/samtel/bizagi/client/impl/CaseBizagiClientImpl.java
+++ bizagi-web/src/main/java/com/samtel/bizagi/client/impl/CaseBizagiClientImpl.java
@@ -1,8 +1,19 @@
 package com.samtel.bizagi.client.impl;
 
 import com.samtel.bizagi.client.CaseBizagiClient;
+import com.samtel.bizagi.common.properties.ServiciosProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CaseBizagiClientImpl implements CaseBizagiClient {
+    private final ServiciosProperties serviciosProperties;
+
+    @Autowired
+    public CaseBizagiClientImpl(ServiciosProperties serviciosProperties) {
+        this.serviciosProperties = serviciosProperties;
+    }
+
     @Override
     public Object createCase(Object request) {
         //TODO Create consume service
--- bizagi-web/src/test/java/com/samtel/bizagi/client/CaseBizagiClientTest.java
+++ bizagi-web/src/test/java/com/samtel/bizagi/client/CaseBizagiClientTest.java
@@ -1,18 +1,24 @@
 package com.samtel.bizagi.client;
 
 import com.samtel.bizagi.client.impl.CaseBizagiClientImpl;
+import com.samtel.bizagi.common.properties.ServiciosProperties;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CaseBizagiClientTest {
     private CaseBizagiClient caseBizagiClient;
+    private ServiciosProperties serviciosProperties;
 
     @Before
     public void setUp() {
-        caseBizagiClient = new CaseBizagiClientImpl();
+        MockitoAnnotations.initMocks(this);
+        serviciosProperties = new ServiciosProperties();
+        serviciosProperties.setUriCrearCaso("www.google.com");
+        caseBizagiClient = new CaseBizagiClientImpl(serviciosProperties);
     }
 
     @Test
@@ -20,5 +26,6 @@ public class CaseBizagiClientTest {
         Object request = new Object();
         Object result = caseBizagiClient.createCase(request);
         Assert.assertNotNull(result);
+        Assert.assertNotNull(serviciosProperties.getUriCrearCaso());
     }
 }
