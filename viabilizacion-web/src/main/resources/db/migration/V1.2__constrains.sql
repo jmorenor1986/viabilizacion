adae6e2ce1038b8ce1290d93f686efaf58f07a88
Jesus Sierra <jnsierrac@gmail.com>
2020-03-22 15:15:07 -0500

    configuracion flyway en viabilizacion-web

---
+++ viabilizacion-web/src/main/resources/db/migration/V1.2__constrains.sql
@@ -0,0 +1,15 @@
+--
+ALTER TABLE cache_usr
+ADD CONSTRAINT fk_cache_usr_log
+FOREIGN KEY (log_id) REFERENCES log
+;
+--
+ALTER TABLE log
+ADD CONSTRAINT fk_log_principal_request
+FOREIGN KEY (principalRequest_id) REFERENCES principal_request
+;
+--
+ALTER TABLE log
+ADD CONSTRAINT fk_log_servicio
+FOREIGN KEY (servicio_id) REFERENCES servicio
+;
