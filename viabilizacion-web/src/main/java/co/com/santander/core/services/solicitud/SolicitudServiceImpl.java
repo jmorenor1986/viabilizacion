674359b147d7eaf520d7e6cd553a8c94ce0af1ba
Jesus Sierra <jnsierrac@gmail.com>
2020-03-30 12:51:25 -0500

    Ajustes para el buen funcionamiento del acceso a base de datos

--- viabilizacion-web/src/main/java/co/com/santander/core/services/solicitud/SolicitudServiceImpl.java
+++ viabilizacion-web/src/main/java/co/com/santander/core/services/solicitud/SolicitudServiceImpl.java
@@ -8,14 +8,6 @@ import co.com.santander.core.domain.solicitud.ClienteValidator;
 import co.com.santander.core.errors.MandatoryFieldException;
 import co.com.santander.core.flow.ValidateRequest;
 import co.com.santander.core.response.ResponseFlow;
-<<<<<<< HEAD
-=======
-import co.com.santander.persistencia.common.FlowOperationEnum;
-import co.com.santander.persistencia.entity.PrincipalRequest;
-import co.com.santander.persistencia.service.LogService;
-import co.com.santander.persistencia.service.PrincipalRequestService;
-import co.com.santander.persistencia.controller.payload.LogPayload;
->>>>>>> 89b0d4c225b317173cc1a53acfd87159f4a9dcbe
 import co.com.santander.ports.primary.solicitud.SolicitudService;
 import co.com.santander.ports.secondary.accesodatos.LogService;
 import co.com.santander.ports.secondary.accesodatos.PrincipalRequestService;
